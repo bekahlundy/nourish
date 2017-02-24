c3e1056397cdb4a03a84f9ad0415e25a133a759d
bekahlundy <lundyrebekah@gmail.com>
2017-02-23 21:39:09 -0700

    Set styles to individual goals

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Goal = ({ handleClick, completed, text }) => {
   return (
     <p
+      className={text.label}
       onClick={ handleClick }
       style={
       { textDecoration: completed ? 'line-through' : 'none' }
