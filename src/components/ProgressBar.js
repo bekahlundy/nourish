6d7b6d063ccc84479bf3185477b9a7d7d876151f
bekahlundy <lundyrebekah@gmail.com>
2017-02-28 21:22:22 -0700

    Actions completely tested

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -13,8 +13,8 @@ const ProgressBar = ({ width, height, physicalNum, emotionalNum, mentalNum, spir
         <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg">
           <g>
             <rect fill="#90C2E7" x={0} y={0} width={physicalWidth} height={height} />
-            <rect fill="#94DDBC" x={physicalWidth} y={0} width={emotionalWidth} height={height} />
-            <rect fill="#EEABC4" x={emotionalWidth + physicalWidth} y={0} width={mentalWidth} height={height} />
+            <rect fill="#EEABC4" x={physicalWidth} y={0} width={emotionalWidth} height={height} />
+            <rect fill="#94DDBC" x={emotionalWidth + physicalWidth} y={0} width={mentalWidth} height={height} />
             <rect fill="#F3DE8A" x={mentalWidth + emotionalWidth + physicalWidth} y={0} width={spiritualWidth} height={height} />
           </g>
         </svg>
